e901140742d8d1cd7c2a593624bb9911fdd6e9fa
ThePythonator <49040244+ThePythonator@users.noreply.github.com>
2021-03-01 17:59:59 +0000

    Tweaked demo.

--- Python/Demo.py
+++ Python/Demo.py
@@ -9,14 +9,15 @@ def render_point(screen, point, colour, radius):
 colours = [
     (255, 255, 255),
     (255, 0, 0),
+    (255, 255, 0),
     (0, 0, 0)
 ]
 
 # BezierCurve data
 nodes = [ # Control nodes
    Point.Point(40, 40),
-   Point.Point(80, 160),
-   Point.Point(160, 80),
+   Point.Point(60, 160),
+   Point.Point(180, 80),
    Point.Point(200, 200)
 ]
 max_t = 2 # seconds
@@ -47,7 +48,7 @@ while True:
     bezier_curve.update(dt)
 
     # Render section
-    screen.fill(colours[2])
+    screen.fill(colours[3])
 
 
     if bezier_curve.is_running():
@@ -55,6 +56,9 @@ while True:
 
     for point in points:
         render_point(screen, point, colours[0], 1)
+        
+    for point in bezier_curve.nodes:
+        render_point(screen, point, colours[2], 1)
         
     render_point(screen, bezier_curve.calculate(), colours[1], 5)
 
